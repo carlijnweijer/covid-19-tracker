d8758758458c9cabb65cce8f42ae0265adeb73ca
carlijnweijer <carlijnweijer@gmail.com>
2020-11-15 21:53:37 +0100

    countries dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,42 @@
 import { FormControl, MenuItem, Select } from "@material-ui/core";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectSummary } from "../store/summary/selectors";
+import { selectSummary, selectCountries } from "../store/summary/selectors";
 import { fetchSummary } from "../store/summary/actions";
 
 export default function Header() {
   const summary = useSelector(selectSummary);
+  const countries = useSelector(selectCountries);
+  const [country, setCountry] = useState("worldwide");
+
   console.log("what is summary", summary);
+  console.log("what is countries", countries);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchSummary());
   }, [dispatch]);
 
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+
+    console.log("what is countrycode", countryCode);
+    setCountry(countryCode);
+  };
+
   return (
     <div className="Header">
       <h1>Covid 19 tracker</h1>
       <FormControl>
-        <Select variant="outlined" value="worldwide">
+        <Select variant="outlined" value={country} onChange={onCountryChange}>
           <MenuItem value="worldwide">Worldwide</MenuItem>
-          {/* {countries.map((country) => {
+          {countries?.map((country) => {
             return (
-              <MenuItem key={country.ISO2} value={country.Country}>
-                {country.Country}
+              <MenuItem key={country.countryCode} value={country.countryCode}>
+                {country.country}
               </MenuItem>
             );
-          })} */}
+          })}
         </Select>
       </FormControl>
     </div>
--- src/store/summary/selectors.js
+++ src/store/summary/selectors.js
@@ -1,1 +1,7 @@
 export const selectSummary = (state) => state.summary.all;
+
+export const selectCountries = (state) => {
+  return state.summary.all.Countries?.map((country) => {
+    return { country: country.Country, countryCode: country.CountryCode };
+  });
+};
